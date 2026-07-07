80fe931342289bbc37c1676f7838557359a2336e
InkTeflix <kumawatjatin94@gmail.com>
2026-07-07 19:05:40 +0530

    Initial Commit

---
+++ Hello_TV.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This is not my first HEllo World!(first was, in C++), but it is a project i am working on will everything is about to collapes.
+#Hello World is the starting of another bad ending. Written by: My dear keyboard(which was 4 keys missing... now.)
+#As much as i would like anyone, idiot enough to try this ocde- more like there luck- i would highly recommend ypu do not execute it and wait for it to finish.
+
+
+# In[ ]:
+
+
+def hello_world():
+
+    you_got_it = '' #you have really got have. give it a try and see your out-shining results, dattebayo...
+    import random
+
+    #we have a few variables, and all of them are very sprcial to me.
+    kanto = random.randint(1, 151)
+    johto = random.randint(1, 251)
+    hoenn = random.randint(1, 386)
+    sinnoh = random.randint(1, 493)
+    unova = random.randint(1, 649)
+    kalos = random.randint(1, 721)
+    alola = random.randint(1, 809)
+    galar = random.randint(1, 905)
+    paldea = random.randint(1, 1025)
+
+    if kanto == 143: #this is my actual fav. cuz the number itself tells that how much I ha- love you... now i am blushing... DAMN IT...
+        you_got_it = you_got_it + 'H'
+
+        if johto == 197: #every rooted 'if' can be taken out, so this become a bit logical.
+            you_got_it = you_got_it + 'e'
+
+            if hoenn == 384: #every number is being matched by the exact randomly generated one from their names.
+                you_got_it = you_got_it + 'l'
+
+                if sinnoh == 445: #what does lost hope looks like
+                    you_got_it = you_got_it + 'l'
+
+                    if unova == 612: #this is my last time ever writting a line of code... before i go to sleep and the clock markes a new day.
+                        you_got_it = you_got_it + 'o'
+
+                        if kalos == 658: #js had to add a space to cover one more reigon
+                            you_got_it = you_got_it + " "
+
+                            if alola == 791: #how does a win taste like...
+                                you_got_it = you_got_it + 'T'
+
+                                if galar == 887:
+                                    you_got_it = you_got_it + 'v' # what does, see the dream come true feels like. I will never know.
+
+                                    if paldea == 937: #even if i am the only one who will ever see this lines, if you ever reach here...
+                                        you_got_it = you_got_it + '?' #well... according to the odds... you might be the only person to ever live on this planet, to do this
+
+
+    return(you_got_it)
+
+tries = 0
+hello_tv = ''
+while hello_tv != 'Hello Tv?':
+    hello_tv = hello_world()
+    tries = tries + 1
+    #print(tries) #if you acutally want to see the number of attemps your system has made thus far, just remove the '#' prior to 'print' (uncomment)
+
+#it is completely upto you weather you belive me or not, but it did indeed took me more than 4 days and nights on first tryI i thought it might be broke.
+print(hello_tv, '\n\nIt took you ', tries, ' to get it right.\nAnd the person who developed it took 74,344,098.') #before my tabs crashed. i never made it through to see the end. T_T
+
